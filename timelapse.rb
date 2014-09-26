668c7c5c34e7c5d779f2a2a0476108344d4bda8c
Jean-Claude Batista <Jean-Claude Batista>
2014-09-26 23:42:21 +0000

    now using raspistill '-tl' param to perform the actual timelapse (instead of manually taking one pic at the time)

--- timelapse.rb
+++ timelapse.rb
@@ -1,14 +1,25 @@
+# sudo gem install sys-filesystem
+require 'sys/filesystem'
 require 'thread'
 require 'pi_piper'
 include PiPiper
 
 # quick and dirty implementation of using push buttons to preview a timelaps
 # on the Raspberry Pi using the Pi Camera and an LCD touch pannel 
+# commands are taken using the raspberry pi
 
 $semaphore = Mutex.new
 $timelapse_started = false
-$wait_time = 5
+$wait_time = 1 # in seconds
 $save_dir = "pics"
+$filename_template = nil
+$max_running_length = 8 # in hours 
+
+def get_remaining_space
+  stat = Sys::Filesystem.stat("/")
+  mb_available = stat.block_size * stat.blocks_available / 1024 / 1024
+  return mb_available
+end
 
 def execute(command)
   puts 'taking preview pic...'
@@ -26,21 +37,55 @@ after :pin => 4, :goes => :high do
   execute("raspistill -w 1920 -h 1080 -o preview.jpg");
 end
 
+def can_proceed?(remaining_space)
+  success = true
+  if remaining_space < 10
+    put "running out of disk space..."
+    success = false
+  end 
+  return success
+end
+
 def thread_func
-  $timelapse_started = true 
-  date = Time.now.strftime("%Y%d%m")
-  counter = 0
-  random = [*0..100].sample
-  
-  while $timelapse_started
-    filename = "./#{$save_dir}/f_r#{random}_#{date}_#{counter}.jpg"         
-    command = "raspistill -w 1920 -h 1080 -n -o #{filename}"
-    puts "#{counter}. Running '#{command}'..."
-    puts `#{command}`
-    counter = counter + 1
-    sleep $wait_time
+  begin
+    while $timelapse_started
+      remaining_space = get_remaining_space
+      if !can_proceed?(remaining_space)
+        stop_timelaspe
+        return
+      end
+      #puts "$filename_template=#{$filename_template}"
+      files = Dir.glob("#{$filename_template}*")
+      puts "image count=#{files.length} remaining space=#{remaining_space}mb"
+      sleep $wait_time + 0.01
+    end
+  rescue
+    puts "An error has occured...", $!, $@
   end
   puts "timelapse stopped!"
+end
+
+def start_timelapse
+  $timelapse_started = true
+  puts "starting timelapse intervall=#{$wait_time} secs for a max of #{$max_running_length} hours..."    
+  date = Time.now.strftime("%Y%d%m")
+  random = [*0..100].sample
+  $filename_template = "./#{$save_dir}/f_r#{random}_#{date}"         
+  filename = "#{$filename_template}_%04d.jpg"
+  wait_time_ms = $wait_time * 1000
+  max_length = $max_running_length * 60 * 60 * 1000
+  command = "raspistill -t #{max_length} -tl #{wait_time_ms} -w 1920 -h 1080 -n -o #{filename}"
+  puts "Running '#{command}'..."
+  fork do
+    `#{command}`
+  end
+  timelapse_thread = Thread.new { thread_func }
+end
+
+def stop_timelapse
+  puts "Stopping timelapse..."  
+    $timelapse_started = false
+    `sudo pkill raspistill`
 end
 
 #watch :pin => 17 do
@@ -48,20 +93,13 @@ after :pin => 17, :goes => :high do
   puts "Pin changed from #{last_value} to #{value}"
   $semaphore.synchronize {
     if $timelapse_started == false 
-      $timelapse_started = true
-      puts "starting timelapse intervall=#{$wait_time} secs..."    
-      timelapse_thread = Thread.new { thread_func }
+      return if !can_proceed?(get_remaining_space)
+      start_timelapse
     else
-      puts "Stopping timelapse..."  
-      $timelapse_started = false
+      stop_timelapse
     end
   }
 end
 
-#Or
-#
-#after :pin => 23, :goes => :high do
-#  puts "Button pressed"
-#  end
-#
+puts "Timelapse thingy started ..."
 PiPiper.wait
